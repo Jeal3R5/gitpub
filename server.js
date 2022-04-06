e67eb8840645dbc07b8371b7467123368428e3fa
Joan Renfroe <joanrenfroe@Joans-MacBook-Pro.local>
2022-04-05 22:59:55 -0700

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const drinks = require("./models/drinks.js");
+
+app.get("/drinks", (req, res) => {
+  res.send();
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to the GitPub App!");
