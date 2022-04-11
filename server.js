9f7816822bfb10d27b39d8c263a0d63955f298f6
Joan Renfroe <joanrenfroe@Joans-MacBook-Pro.local>
2022-04-10 17:26:34 -0700

    added some superbasic css

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const app = express();
 const PORT = 3000;
 const drinks = require("./models/drinks.js");
 
+// Root Route - Welcome
 app.get("/", (req, res) => {
   res.send("Welcome to the GitPub App!");
 });
 
 // Index Route - show all of the fruits
 app.get("/drinks/", (req, res) => {
-  res.render("index.ejs", { drinks: drinks });
+  res.render("drinks_index.ejs", { drinks: drinks });
+ 
 });
 
-// Show Route - show a specific fruit
-app.get("/fruits/:id", (req, res) => {
-  res.render("show.ejs", { fruit: fruits[req.params.id] });
-});
+// // Show Route - show a specific fruit
+// app.get("/drinks/:id", (req, res) => {
+//   res.render("show.ejs", { drinks: drinks[req.params.id] });
+// });
 
 app.listen(3000, () => {
   console.log(`Listening on port ${PORT}`);
